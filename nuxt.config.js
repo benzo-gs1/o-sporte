3369b5d2b1e9a0ee84f5f57b3dd88104d1e0db7d
RayDarar <dfqgth400@gmail.com>
2020-10-29 06:13:37 +0600

    work: i18n enabled

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,54 @@
 export default {
-  // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+  target: "static",
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'app',
+    title: "OSporte",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Информационный портал о спорте",
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-  ],
+  buildModules: ["@nuxtjs/eslint-module"],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-  ],
+  modules: ["@nuxtjs/axios", "@nuxtjs/pwa", "nuxt-i18n"],
 
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  i18n: {
+    locales: [
+      {
+        code: "kz",
+        iso: "kz-KZ",
+      },
+      {
+        code: "ru",
+        iso: "ru-RU",
+      },
+    ],
+    baseUrl: "https://1sport.kz",
+    seo: true,
+    defaultLocale: "ru",
+    vueI18n: {
+      fallbackLocale: "ru",
+      messages: {
+        ru: require("./locales/ru.json"),
+        kz: require("./locales/kz.json"),
+      },
+    },
+  },
+
   build: {},
-}
+};
