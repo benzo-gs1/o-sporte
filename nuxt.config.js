e63b3dea6269f76024e67d386bbe0a31acfed3e5
RayDarar <dfqgth400@gmail.com>
2020-12-06 18:10:38 +0600

    fix: titles

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   },
 
   head: {
-    title: "1 Спорт",
+    title: "Первый Спортивный",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
