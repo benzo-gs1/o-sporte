3a43fb94ac5a42be7c9a24de7260007ffe2081a6
RayDarar <dfqgth400@gmail.com>
2020-12-15 17:31:32 +0600

    fix(nuxt.config): target mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
     port: 8080,
     host: "localhost",
   },
-  router: {},
+  mode: "spa",
   generate: {
     async routes() {
       const baseUrl = `https://api.1sport.kz/wp-json/wp/v2/posts?_fields=id,slug&per_page=100`;
